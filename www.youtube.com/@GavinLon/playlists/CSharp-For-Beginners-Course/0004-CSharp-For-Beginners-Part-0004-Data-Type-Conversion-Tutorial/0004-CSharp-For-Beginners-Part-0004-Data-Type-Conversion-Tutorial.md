b8fc7247198345795d5de539b01cc05ddc6eecf2
Ramu Mohan Srinivasan Radhakrishnan <ramumsrk02071981@gmail.com>
2024-08-04 23:31:46 +0530

    0004 C# for Beginners - Part 0004 - C# Data Type Conversion Tutorial
    https://www.youtube.com/watch?v=NF4lyA1yx8Y&list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di&index=4
    
    C# for Beginners Course
    
    Playlist https://www.youtube.com/playlist?list=PL4LFuHwItvKbneXxSutjeyz6i1w32K6di
    
    On branch main-CSharp-Programming-Language-dotNET-Core
    Your branch is up to date with 'origin-CSharp-Programming-Language-dotNET-Core/main-CSharp-Programming-Language-dotNET-Core'.
    
    Changes to be committed:
    new file:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/0004-CSharp-For-Beginners-Part-0004-Data-Type-Conversion-Tutorial/0004-CSharp-For-Beginners-Part-0004-Data-Type-Conversion-Tutorial.md
    new file:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/logs/DataTypeConversionExample/dotnet_build_--verbosity_diagnostic.txt
    new file:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/logs/DataTypeConversionExample/dotnet_new_create_console_--verbosity_diagnostic_--framework_net8.0_--language_csharp_--langVersion_12_--name_DataTypeConversionExample_--output.txt
    new file:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/src/csharp_for_beginners_course/DataTypeConversionExample/DataTypeConversionExample.cs
    new file:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/src/csharp_for_beginners_course/DataTypeConversionExample/DataTypeConversionExample.csproj
    modified:   www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/src/csharp_for_beginners_course/csharp_for_beginners_course.sln

---
+++ www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/0004-CSharp-For-Beginners-Part-0004-Data-Type-Conversion-Tutorial/0004-CSharp-For-Beginners-Part-0004-Data-Type-Conversion-Tutorial.md
@@ -0,0 +1,1 @@
+# 0004 C# for Beginners - Part 0004 - C# Data Type Conversion Tutorial
---
+++ www.youtube.com/@GavinLon/playlists/CSharp-For-Beginners-Course/src/csharp_for_beginners_course/DataTypeConversionExample/DataTypeConversionExample.cs
@@ -0,0 +1,15 @@
+﻿namespace DataTypeConversionExample
+{
+  class DataTypeConversionExample
+  {
+    static void Main(string[] arguments)
+    {
+        short _sampleShortLiteral = byte.MinValue;
+        System.Console.WriteLine($"{Environment.NewLine}Value of identifier '_sampleShortLiteral' is '{_sampleShortLiteral}'");        
+        //byte _sampleByteLiteral = short.MinValue;
+        byte _sampleByteLiteral = System.Convert.ToByte(short.MinValue);
+        System.Console.WriteLine($"{Environment.NewLine}Value of identifier '_sampleByteLiteral' is '{_sampleByteLiteral}'");
+      return;
+    }
+  }
+}
